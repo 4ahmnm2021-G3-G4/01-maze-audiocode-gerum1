adf3ccef68fc77fcada042631b2f1bad98733f32
“gerum1” <richard.gerum02@htl-salzburg.ac.at>
2021-05-31 09:58:58 +0200

    Sonnenpickup Animation anfangen. Rotate und Auf/Abbewegung erstellen

---
+++ Assets/MyGame/Scripts/PickUpRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpRotate : MonoBehaviour
+{
+    float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+
+        if (transform.position.y >= -1f)
+        {
+            transform.position = new Vector3(transform.position.x, 1,22 * transform.position.y + 1 * speed * Time.deltaTime, transform.position.z);
+        }
+        else if (transform.position.y <= 0f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1 * speed * Time.deltaTime, transform.position.z);
+        }
+    }
+}
