262e33396678a7b750c2d8cba6fb2809526438fe
“gerum1” <richard.gerum02@htl-salzburg.ac.at>
2021-06-01 13:15:58 +0200

    Animation für Pickup fertig gemacht (30min HÜ)

--- Assets/MyGame/Scripts/PickUpRotate.cs
+++ Assets/MyGame/Scripts/PickUpRotate.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 
 public class PickUpRotate : MonoBehaviour
 {
-    float speed = 5.0f;
-    // Start is called before the first frame update
-    void Start()
+    float speed = 50f;
+    bool moveUpBool;
+    private void Start()
     {
-
+        moveUpBool = true;
     }
-
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        if (moveUpBool)
+        {
+            MoveUp();
+        }
+        else
+        {
+            MoveDown();
+        }
+    }
+    void MoveUp (){
+        MoveSun(0.01f);
+        if (transform.position.y >= 0f)
+        {
+            moveUpBool = false;
+        }
+    }
+    void MoveDown()
+    {
+        MoveSun(-0.01f);
+        if (transform.position.y <= -0.5f)
+        {
+            moveUpBool = true;
+        }
+    }
+    void MoveSun(float yValue)
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y + yValue, transform.position.z);
 
-        if (transform.position.y >= -1f)
-        {
-            transform.position = new Vector3(transform.position.x, 1,22 * transform.position.y + 1 * speed * Time.deltaTime, transform.position.z);
-        }
-        else if (transform.position.y <= 0f)
-        {
-            transform.position = new Vector3(transform.position.x, transform.position.y + 1 * speed * Time.deltaTime, transform.position.z);
-        }
     }
 }
