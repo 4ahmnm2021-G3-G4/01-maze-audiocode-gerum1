3f4709b372c439646d67ff91041c4f6d1b3eb61f
yslindner <marco.lindner@htl-salzburg.ac.at>
2021-06-21 09:25:45 +0200

    first testing on VR

--- Assets/MyGame/Scripts/PlayerCatchPickUp.cs
+++ Assets/MyGame/Scripts/PlayerCatchPickUp.cs
@@ -13,7 +13,7 @@ public class PlayerCatchPickUp : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        audio5.Play();
+       
         sunCounter = 0;
     }
     private void OnTriggerEnter(Collider other)
